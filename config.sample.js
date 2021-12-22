79ec181cc1752ca0f26da05f9ad73c8821dac813
Alex63su <varava.as93@gmail.com>
2021-12-22 16:39:28 +0200

    Revert "Create config.sample.js"
    This reverts commit 94728322e738359db09fc09c531ba71b62b3d57e.

--- config.sample.js
+++ config.sample.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'a9872b35789f9a1479b764016b6e488f7dcbc396b94092cb2ce1a374ff8b0557e86b371a9ac6600f55bee815c0b746c8',
+    token: '',
     strategies: {
       99: {condition: 'above', target: 98.99, increment: 1.03/100+1, endurance: 1200},
       51: {condition: 'above', target: 49, increment: 110/100+1, endurance: 24},
