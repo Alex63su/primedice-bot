bb436b717b6ccd5c12b8b47b9b89c08ea3d2302b
Alex63su <85257614+Alex63su@users.noreply.github.com>
2021-12-22 15:43:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '36f56403a3ef44c193d157c90049539dcc6eddebc88207b5876e78ab7918a6f18f188338ef06d0dab797b0f3d5b7ccf1',
+    token: 'a9872b35789f9a1479b764016b6e488f7dcbc396b94092cb2ce1a374ff8b0557e86b371a9ac6600f55bee815c0b746c8',
     strategies: {
       99: {condition: 'above', target: 98.99, increment: 1.03/100+1, endurance: 1200},
       51: {condition: 'above', target: 49, increment: 110/100+1, endurance: 24},
