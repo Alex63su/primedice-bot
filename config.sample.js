ce9f981f52f9d670d6cb21b6049311d6bc00948b
Alex63su <85257614+Alex63su@users.noreply.github.com>
2021-12-22 13:50:50 +0200

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '',
+    token: '36f56403a3ef44c193d157c90049539dcc6eddebc88207b5876e78ab7918a6f18f188338ef06d0dab797b0f3d5b7ccf1',
     strategies: {
       99: {condition: 'above', target: 98.99, increment: 1.03/100+1, endurance: 1200},
       51: {condition: 'above', target: 49, increment: 110/100+1, endurance: 24},
