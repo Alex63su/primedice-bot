f2bfb19495e99d90cd965613f8e8380223f2b209
Alex63su <85257614+Alex63su@users.noreply.github.com>
2021-12-22 13:32:29 +0200

    Update and rename config.sample.js to config..js

--- config..js
+++ config..js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '',
+    token: 'a048260c424412d0502596169e07475ee3910698f50f3dddbc56c59d28ec6e40b69f8cb04fc6d48e19fc32e288da5334',
     strategies: {
       99: {condition: 'above', target: 98.99, increment: 1.03/100+1, endurance: 1200},
       51: {condition: 'above', target: 49, increment: 110/100+1, endurance: 24},
